738063f2cc672175d49fc63c0b071fbe6b5ec9b2
Wesley Chen <wesleychen@wesjchenmacm3.T-mobile.com>
2024-01-25 19:59:26 -0600

    add conftest hooks

---
+++ conftest.py
@@ -0,0 +1,24 @@
+import pytest
+import requests
+
+# Calling a fixture to set up the api auth token
+@pytest.fixture(scope='session', autouse=True)
+def setup_api_auth(request, pytestconfig):
+    api_key = pytestconfig.getoption('--api-key')
+    api_token = pytestconfig.getoption('--api-token')
+
+    # Authentification setup
+    headers = {
+        'Authorization': f'Bearer{api_token}',
+        'X-API-Key': api_key,
+    }
+    request.config.cache.set('api_headers', headers, scope='session')
+
+# Session setup
+@pytest.hookimpl
+def pytest_sessionstart(session):
+    print("Test session is setting up")
+
+@pytest.hookimpl
+def pytest_sessionend(session):
+    print("Test session is tearing down")
