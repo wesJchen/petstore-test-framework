772047f393686beedc8a763c82311ca77a60a3d8
Wesley Chen <wesleychen@wesjchenmacm3.T-mobile.com>
2024-01-30 02:01:06 -0600

    add request handler for get_pet endpoint

--- services/petstore_service.py
+++ services/petstore_service.py
@@ -2,13 +2,19 @@ import requests
 
 class PetStoreService:
 
-    base_url = 'https://petstore.swagger.io/oauth/authorize'
+    base_url = 'https://petstore.swagger.io'
 
-    def get_pet(self, pet_id):
+    @staticmethod
+    def get_pet(pet_id:int) -> str:
         """
-        Send a get request to petstore
+        Send a get request to petstore.swagger.io/v2/pet + pet_id
         :param pet_id: integer of a pet ID
         :return: Status code and body of valid identifying fields for the pet ID requested
         """
-        response = requests.get(PetStoreService.base_url, pet_id)
-        return response
+        status,response = requests.get(f'{PetStoreService.base_url}/v2/pet/{pet_id}')
+        return status, response
+    
+# if __name__ == "__main__":
+    
+#     status, response = PetStoreService.get_pet(9)
+#     print(status)
