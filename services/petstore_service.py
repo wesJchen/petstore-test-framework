c984b3a38fc030b72759d82409dca9259bce836d
Wesley Chen <wesjchen@gmail.com>
2024-02-01 00:35:51 -0600

    update service file

--- services/petstore_service.py
+++ services/petstore_service.py
@@ -5,16 +5,17 @@ class PetStoreService:
     base_url = 'https://petstore.swagger.io'
 
     @staticmethod
-    def get_pet(pet_id:int) -> str:
+    def get_pet(pet_id:int) -> tuple[str, dict]:
         """
         Send a get request to petstore.swagger.io/v2/pet + pet_id
         :param pet_id: integer of a pet ID
         :return: Status code and body of valid identifying fields for the pet ID requested
         """
-        status,response = requests.get(f'{PetStoreService.base_url}/v2/pet/{pet_id}')
+        status, response = requests.get(f'{PetStoreService.base_url}/v2/pet/{pet_id}')
         return status, response
     
-# if __name__ == "__main__":
+if __name__ == "__main__":
     
-#     status, response = PetStoreService.get_pet(9)
-#     print(status)
+    status, response = PetStoreService.get_pet(0)
+    print(status)
+    print(response)
--- tests/functional_tests/test_pet.py
+++ tests/functional_tests/test_pet.py
@@ -7,12 +7,11 @@ class PetClass:
     def test_get_pet(self):
         
         ### ARRANGE ###
-        ...
+        num_pets = 10
 
         ### ACT ###
-        ...
+        for id in range(num_pets):
+            status_code, response = PetStoreService.get_pet(id)
 
         ### ASSERT ###
-        ...
-
-
+            assert response["sample_field"] == "sample_test_data"
